4912177397ceddd77cb75b88d0994cb998087dc2
Erik Neemann <38330621+eneemann@users.noreply.github.com>
2023-06-06 11:58:16 -0600

    feat: add credentials_template.py
    template for credentials file that is needed to assign connection files and base directory variables

---
+++ credentials_template.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+file to hold connection information
+
+update variables and change filename to credentials.py
+
+"""
+
+SGID = r'update_me' #: Full path to sde connection file that can access all of SGID
+BASE_DIRECTORY = r'C:\Temp'  #: Base Directory for storing data
+
+CONNECTIONS = {
+    'SOCIETY' : r'update_me' #: Full path to sde connection file that can edit SOCIETY
+}
